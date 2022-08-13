ccee1a3c79b8181d34f98b8dd626100c1af206ff
‘terupro’ <terupro911@gmail.com>
2022-08-13 15:06:38 +0900

    ⚡️ バックエンドとフロントエンド接続(APIを叩く/画像のアップロード)

---
+++ routes/upload.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const multer = require("multer");
+
+// ファイルの保存先、ファイルの名前
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "/public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+// 画像アップロード用API
+router.post("/", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("画像アップロードに成功しました");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const PORT = 4000;
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const uploadRoute = require("./routes/upload");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 // データベース接続
@@ -18,10 +20,12 @@ mongoose
   });
 
 // ミドルウェア
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/upload", uploadRoute);
 
 // app.get("/", (req, res) => {
 //   res.send("hello express");
