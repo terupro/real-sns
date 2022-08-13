c6992d0c34786419acc9266b04a6e0b6776a8182
‘terupro’ <terupro911@gmail.com>
2022-08-13 10:57:19 +0900

    ⚡️ バックエンドとフロントエンド接続(ログイン・新規登録編)

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,7 @@ router.post("/login", async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (!user) return res.status(404).send("ユーザーが見つかりません");
     const vailedPassword = req.body.password === user.password;
-    if (!vailedPassword) return res.status(400).json("パソワードが違います");
+    if (!vailedPassword) return res.status(400).json("パスワードが違います");
     return res.status(200).json(user);
   } catch (err) {
     return res.status(500).json(err);
